f9b5c7db97b28df98d449720f4e2185f1add208a
mingli <mingli.yuan@gmail.com>
2020-08-19 16:08:38 +0800

    add tests

--- src/train_mm.py
+++ src/train_mm.py
@@ -76,7 +76,7 @@ class MMModel(nn.Module):
     def forward(self, input):
         data = self.unet(input / 255 * 2 - 1.0)
         value = th.sigmoid(data)
-        value = th.sum(value.reshape(-1, 7, 64, 64), dim=1)
+        value = th.clamp(th.sum(value.reshape(-1, 7, 64, 64), dim=1), max=1.0, min=0)
         value = value.reshape(-1, 10, 64, 64)
         return value * 255
 
