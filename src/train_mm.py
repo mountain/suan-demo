5bc84236b6566e1b02263d9452b7dc67ebe1e181
mingli <mingli.yuan@gmail.com>
2020-08-19 13:24:27 +0800

    add tests

--- src/train_mm.py
+++ src/train_mm.py
@@ -68,7 +68,7 @@ class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.unet = UNet(10, 10, normalizor='batch', spatial=(64, 64), layers=6, ratio=0,
-                            vblks=[4, 4, 4, 4, 4, 4], hblks=[2, 2, 2, 2, 2, 2],
+                            vblks=[2, 2, 2, 2, 2, 2], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             block=HyperBottleneck, relu=nn.ReLU(), final_normalized=False)
 
