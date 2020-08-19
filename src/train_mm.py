b3d29863e56587252b3cdba760cbce7e6219c638
mingli <mingli.yuan@gmail.com>
2020-08-19 13:39:56 +0800

    add tests

--- src/train_mm.py
+++ src/train_mm.py
@@ -68,9 +68,9 @@ test_loader = torch.utils.data.DataLoader(
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.unet = UNet(10, 10, normalizor='batch', spatial=(64, 64), layers=6, ratio=0,
-                            vblks=[2, 2, 2, 2, 2, 2], hblks=[1, 1, 1, 1, 1, 1],
-                            scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
+        self.unet = UNet(10, 10, normalizor='batch', spatial=(64, 64), layers=4, ratio=0,
+                            vblks=[4, 4, 4, 4], hblks=[2, 2, 2, 2],
+                            scales=[-1, -1, -1, -1], factors=[1, 1, 1, 1],
                             block=HyperBottleneck, relu=Swish(), final_normalized=False)
 
     def forward(self, input):
