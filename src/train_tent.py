4cb2496e093d2e37313abfa54fe2f1f86580b167
Mingli Yuan <mingli.yuan@gmail.com>
2020-08-23 07:53:08 +0800

    change noise to normal distributed

--- src/train_tent.py
+++ src/train_tent.py
@@ -70,7 +70,7 @@ class LearningModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.unet = UNet(2, 10, normalizor='batch', spatial=(32, 32), layers=5, ratio=0,
-                            vblks=[4, 4, 4, 4, 4], hblks=[4, 4, 4, 4, 4],
+                            vblks=[2, 2, 2, 2, 2], hblks=[2, 2, 2, 2, 2],
                             scales=[-1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1],
                             block=HyperBottleneck, relu=CappingRelu(), final_normalized=True)
 
