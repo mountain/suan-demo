fa33e02a78aa118d92702d2999dc0639a5da9fc9
Mingli Yuan <mingli.yuan@gmail.com>
2020-08-22 23:55:27 +0800

    typo

--- scripts/gen_tent.py
+++ scripts/gen_tent.py
@@ -10,18 +10,18 @@ for ix in range(800):
     for jx in range(12):
         z = (z < 0.5) * (2 * z) + (z >= 0.5) * (2 - 2 * z) + np.random.rand(32, 32) * 0.01
         z = z * (z < 1.0) * (z > 0.0)
-        cv2.imwrite('data/train/z%03d_%02d.png' % (ix, jx), z * 255)
+        cv2.imwrite('data/tent/train/z%03d_%02d.png' % (ix, jx), z * 255)
 
 for ix in range(200):
     z = np.random.rand(32, 32)
     for jx in range(12):
         z = (z < 0.5) * (2 * z) + (z >= 0.5) * (2 - 2 * z) + np.random.rand(32, 32) * 0.01
         z = z * (z < 1.0) * (z > 0.0)
-        cv2.imwrite('data/validation/z%03d_%02d.png' % (ix, jx), z * 255)
+        cv2.imwrite('data/tent/validation/z%03d_%02d.png' % (ix, jx), z * 255)
 
 for ix in range(500):
     z = np.random.rand(32, 32)
     for jx in range(12):
         z = (z < 0.5) * (2 * z) + (z >= 0.5) * (2 - 2 * z) + np.random.rand(32, 32) * 0.01
         z = z * (z < 1.0) * (z > 0.0)
-        cv2.imwrite('data/test/z%03d_%02d.png' % (ix, jx), z * 255)
+        cv2.imwrite('data/tent/test/z%03d_%02d.png' % (ix, jx), z * 255)
