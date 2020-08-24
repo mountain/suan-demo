19a4d46a83b1554aadd4c696e31cb100be02a514
mingli <mingli.yuan@gmail.com>
2020-08-24 11:39:46 +0800

    add parabola

---
+++ src/dataset/chaos_parabola.py
@@ -0,0 +1,27 @@
+import numpy as np
+import torch.utils.data as data
+
+mu = 2
+
+class ChaosParabolaDataSet(data.Dataset):
+    def __init__(self, length):
+        self.length = length
+
+    def __getitem__(self, index):
+        z = np.random.rand(1, 32, 32) * 2 - 1
+        seq = [z]
+        for jx in range(11):
+            z = z + np.random.normal(scale=0.001, size=(32, 32))
+            z = z * (z > -1.0) * (z < 1.0)
+            z = 1 - mu * z * z
+            seq.append(z.reshape(1, 32, 32))
+        seq = np.concatenate(seq, axis=0)
+        return seq[0:2], seq[2:12]
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        return fmt_str
