68628a2240218050addd81cdf042a2921848fa87
mingli <mingli.yuan@gmail.com>
2020-08-25 16:28:48 +0800

    fix tent

--- src/blks/direct.py
+++ src/blks/direct.py
@@ -1,6 +1,8 @@
 import torch as th
 import torch.nn as nn
 
+from leibniz.unet.senet import SELayer
+
 
 class DirectBlocks(nn.Module):
     extension = 1
@@ -26,13 +28,18 @@ class DirectBlocks(nn.Module):
         self.conv2 = self.conv(dim, dim, kernel_size=3, padding=1, bias=False)
         self.conv3 = self.conv(dim, dim, kernel_size=3, padding=1, bias=False)
 
+        self.se_a = SELayer(dim)
+        self.se_b = SELayer(dim)
+
     def forward(self, x):
         a = self.conv0(x)
         a = self.relu(a)
         a = self.conv1(a)
+        a = self.se_a(a)
 
         b = self.conv2(x)
         b = self.relu(b)
         b = self.conv3(b)
+        b = self.se_b(b)
 
         return a * x + b
