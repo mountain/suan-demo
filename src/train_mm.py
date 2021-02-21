4ba3bfd0c8638a94c8c839c100b725ccff9221c2
mingli <mingli.yuan@gmail.com>
2021-02-21 14:52:59 +0800

    correct formula

--- src/train_mm.py
+++ src/train_mm.py
@@ -75,7 +75,7 @@ class MMModel(nn.Module):
         self.oconv = nn.Conv2d(20, 10, kernel_size=3, padding=1)
         self.dropout = nn.Dropout2d(p=0.5)
 
-        self.enc = resunet(10, 160, block=HyperBottleneck, layers=6, ratio=-1,
+        self.enc = resunet(10, 160, block=HyperBottleneck, layers=6, ratio=-2,
                 vblks=[2, 2, 2, 2, 2, 2], hblks=[3, 3, 3, 3, 3, 3],
                 scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                 spatial=(64, 64))
