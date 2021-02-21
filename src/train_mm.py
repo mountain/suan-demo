correct formula

src/train_mm.py
@@ -96,7 +96,7 @@ class MMModel(nn.Module):
             uparam = flow[:, :, ix, 3]
             vparam = flow[:, :, ix, 4]
             wparam = flow[:, :, ix, 5]
-            output = (oprand + aparam * uparam) * th.exp(mparam * vparam) * th.exp(th.exp(pparam * wparam))
+            output = (oprand + aparam * uparam) * th.exp(mparam * vparam * th.exp(pparam * wparam))
             if ix < 2 - 1:
                 output = self.dropout(output)
 