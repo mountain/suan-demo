correct formula

src/train_mmg.py
@@ -106,7 +106,7 @@ class Generator(nn.Module):
         self.dropout = nn.Dropout2d(p=0.5)
 
         self.enc = resunet(10, 160, block=HyperBottleneck, layers=6, ratio=-1,
-                vblks=[1, 1, 1, 1, 1, 1], hblks=[3, 3, 3, 3, 3, 3],
+                vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                 scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                 spatial=(64, 64))
 