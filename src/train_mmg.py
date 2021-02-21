correct formula

src/train_mmg.py
@@ -184,7 +184,7 @@ def train(epoch):
         optimizer_G.zero_grad()
 
         # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (input.shape[0], 10))))
+        z = Variable(Tensor(np.random.normal(0, 1, (input.shape[0], 10, 64, 64))))
 
         # Generate a batch of images
         result = generator(input, z)