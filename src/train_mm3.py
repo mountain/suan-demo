428f62f3e049ed32358d5cde0bc63d2a566f9e7f
mingli <mingli.yuan@gmail.com>
2021-02-24 12:00:26 +0800

    fix channel

--- src/train_mm3.py
+++ src/train_mm3.py
@@ -75,7 +75,7 @@ class MMModel(nn.Module):
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
-        self.dec = resunet(40, 10, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-2,
+        self.dec = resunet(40, 10, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-4,
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64), final_normalized=True)
