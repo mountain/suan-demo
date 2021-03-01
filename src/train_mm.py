61f3d538a0cb3bc5a93a870f174b2168d41306c2
Mingli Yuan <mingli.yuan@gmail.com>
2021-03-01 20:38:21 +0800

    by rnn

--- src/train_mm.py
+++ src/train_mm.py
@@ -78,7 +78,7 @@ class HypTubeRNN(nn.Module):
         self.out_channels = out_channels
         self.steps = steps
 
-        self.enc = resunet(in_channels, 6 * hidden_channels, normalizor=normalizor, spatial=spatial, layers=layers, ratio=ratio,
+        self.enc = resunet(in_channels, 10 * hidden_channels, normalizor=normalizor, spatial=spatial, layers=layers, ratio=ratio,
                 vblks=vblks, hblks=hblks, scales=scales, factors=factors, block=block, relu=relu, attn=attn, final_normalized=False)
 
         self.dec = resunet(hidden_channels, out_channels, normalizor=normalizor, spatial=spatial, layers=layers, ratio=ratio,
@@ -90,12 +90,12 @@ class HypTubeRNN(nn.Module):
 
         flow = self.enc(input)
         flow, uparam, vparam = flow[:, 2*hc:], flow[:, 0:hc], flow[:, hc:2*hc]
-        flow = flow.view(-1, hc, 2, 2, w, h)
+        flow = flow.view(-1, hc, 4, 2, w, h)
 
         result = []
-        output = th.zeros(b, hc, w, h, device=input.device)
         for jx in range(self.steps):
-            for ix in range(2):
+            output = th.zeros(b, hc, w, h, device=input.device)
+            for ix in range(4):
                 aparam = flow[:, :, ix, 0]
                 mparam = flow[:, :, ix, 1]
                 output = (output + aparam * uparam) * (1 + mparam * vparam)
@@ -107,7 +107,7 @@ class HypTubeRNN(nn.Module):
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn = HypTubeRNN(10, 4, 1, 10, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-2,
+        self.rnn = HypTubeRNN(10, 6, 1, 10, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-2,
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
