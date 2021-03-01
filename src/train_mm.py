7097b0032a1d093a3a2b66160627372a37060f8d
Mingli Yuan <mingli.yuan@gmail.com>
2021-03-02 02:38:11 +0800

    typo

--- src/train_mm.py
+++ src/train_mm.py
@@ -129,7 +129,7 @@ class HypTubeRNN(nn.Module):
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn = HypTubeRNN(10, 4, 4, 11, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-3,
+        self.rnn = HypTubeRNN(10, 8, 4, 11, block=HyperBottleneck, relu=CappingRelu(), layers=6, ratio=-3,
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
