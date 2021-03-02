2175c0499b1ea11cfd76ed4702aca09bea36eff4
mingli <mingli.yuan@gmail.com>
2021-03-02 10:37:18 +0800

    add mm gen

--- src/train_mm.py
+++ src/train_mm.py
@@ -7,13 +7,11 @@ import numpy as np
 import torch
 import torch as th
 import torch.nn as nn
-import torch.nn.functional as F
 import cv2
 
 from pathlib import Path
 from skimage.metrics import structural_similarity as ssim
-from leibniz.unet.cbam import CBAM
-from leibniz.unet import resunet
+from leibniz.hyptub.base import StepwiseHypTube
 from leibniz.unet.hyperbolic import HyperBottleneck
 
 from dataset.moving_mnist import MovingMNIST
@@ -68,48 +66,10 @@ test_loader = torch.utils.data.DataLoader(
                 shuffle=True)
 
 
-class HypTubeRNN(nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, steps, block=HyperBottleneck, relu=nn.ReLU(inplace=True), attn=None, normalizor='batch',
-                 layers=4, ratio=-2, spatial=(256, 256), vblks=[1, 1, 1, 1], hblks=[1, 1, 1, 1], scales=[-2, -2, -2, -2], factors=[1, 1, 1, 1],
-                 final_normalized=False):
-        super().__init__()
-        self.in_channels = in_channels
-        self.hidden_channels = hidden_channels
-        self.out_channels = out_channels
-        self.steps = steps
-
-        self.cbam = CBAM(hidden_channels, conv=nn.Conv2d)
-
-        self.enc = resunet(in_channels, (4 * steps + 2) * hidden_channels, normalizor=normalizor, spatial=spatial, layers=layers, ratio=ratio,
-                vblks=vblks, hblks=hblks, scales=scales, factors=factors, block=block, relu=relu, attn=attn, final_normalized=False)
-
-        self.dec = resunet(hidden_channels, out_channels, normalizor=normalizor, spatial=spatial, layers=layers, ratio=ratio,
-                vblks=vblks, hblks=hblks, scales=scales, factors=factors, block=block, relu=relu, attn=attn, final_normalized=final_normalized)
-
-    def forward(self, input):
-        b, c, w, h = input.size()
-        hc = self.hidden_channels
-
-        flow = self.enc(input)
-        flow, uparam, vparam = flow[:, 2*hc:], flow[:, 0:hc], flow[:, hc:2*hc]
-        flow = flow.view(-1, hc, self.steps, 2, 2, w, h)
-
-        result = []
-        for jx in range(self.steps):
-            output = th.zeros(b, hc, w, h, device=input.device)
-            for ix in range(2):
-                aparam = flow[:, :, jx, ix, 0]
-                mparam = flow[:, :, jx, ix, 1]
-                output = (output + aparam * uparam) * (1 + mparam * vparam)
-            result.append(self.dec(self.cbam(output)))
-
-        return th.cat(result, dim=1)
-
-
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn = HypTubeRNN(10, 8, 1, 10, block=HyperBottleneck, relu=nn.ReLU(inplace=True), layers=6, ratio=-3,
+        self.rnn = StepwiseHypTube(10, 8, 1, 10, block=HyperBottleneck, relu=nn.ReLU(inplace=True), layers=6, ratio=-3,
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
