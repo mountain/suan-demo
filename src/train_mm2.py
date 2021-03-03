7286bf0422e92e41c1cd8ee727ffdb2237814899
mingli <mingli.yuan@gmail.com>
2021-03-03 19:28:25 +0800

    add mm gen

--- src/train_mm2.py
+++ src/train_mm2.py
@@ -68,6 +68,38 @@ test_loader = torch.utils.data.DataLoader(
                 shuffle=True)
 
 
+class StepwiseHypTube2(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, steps, encoder, decoder, **kwargs):
+        super().__init__()
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
+        self.steps = steps
+
+        self.enc = encoder(in_channels, (4 * steps + 2) * hidden_channels, **kwargs)
+        self.dec = decoder(hidden_channels, out_channels * steps, **kwargs)
+
+    def forward(self, input):
+        b, c, w, h = input.size()
+        hc = self.hidden_channels
+
+        flow = self.enc(input)
+        flow, uparam, vparam = flow[:, 2*hc:], flow[:, 0:hc], flow[:, hc:2*hc]
+        flow = flow.view(-1, hc, self.steps, 2, 2, w, h)
+
+        result = []
+        for jx in range(self.steps):
+            output = th.zeros(b, hc, w, h, device=input.device)
+            for ix in range(2):
+                aparam = flow[:, :, jx, ix, 0]
+                mparam = flow[:, :, jx, ix, 1]
+                output = (output + aparam * uparam) * (1 + mparam * vparam)
+            result.append(output)
+
+        result = th.cat(result, dim=1)
+        return self.dec(result)
+
+
 class Encoder(nn.Module):
     def __init__(self, in_channels, out_channels, channels_per_step_in=1, channels_per_step_out=4):
         super().__init__()
@@ -109,7 +141,7 @@ class Decoder(nn.Module):
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.tube = hyptub_stepwise(10, 4, 1, 10, encoder=Encoder, decoder=Decoder)
+        self.tube = StepwiseHypTube2(10, 4, 1, 10, encoder=Encoder, decoder=Decoder)
 
     def forward(self, input):
         input = input / 255.0
