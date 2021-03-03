fc0cd897cce6280a33bdf00830b6232d082b9990
mingli <mingli.yuan@gmail.com>
2021-03-03 15:40:33 +0800

    add mm gen

--- src/train_mm.py
+++ src/train_mm.py
@@ -101,7 +101,7 @@ class LeveledHypTube(nn.Module):
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.tube = LeveledHypTube(3, 10, 10, 2, encoder=resunet, decoder=resunet, propagator=resunet,
+        self.tube = LeveledHypTube(3, 6, 10, 2, encoder=resunet, decoder=resunet, propagator=resunet,
                             block=HyperBottleneck, relu=nn.ReLU(), ratio=-1, layers=3,
                             vblks=[1, 1, 1], hblks=[1, 1, 1],
                             scales=[-2, -2, -2], factors=[2, 2, 2],
