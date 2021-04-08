3574ca42341d0bb85271137c0b280cb13444d527
mingli <mingli.yuan@gmail.com>
2021-04-08 17:25:19 +0800

    fix mm

--- src/train_mm.py
+++ src/train_mm.py
@@ -71,7 +71,7 @@ class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.unet = resunet(10, 10, block=HyperBottleneck, relu=CappingRelu(), ratio=-1, layers=6,
-                            vblks=[6, 6, 6, 6, 6, 6], hblks=[0, 0, 0, 0, 0, 0],
+                            vblks=[6, 6, 6, 6, 6, 6], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
 
