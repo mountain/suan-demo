67964f502d575dc1809607b4313500e0b4b61f12
mingli <mingli.yuan@gmail.com>
2021-04-08 17:35:12 +0800

    fix mm

--- src/train_mm.py
+++ src/train_mm.py
@@ -70,7 +70,7 @@ test_loader = torch.utils.data.DataLoader(
 class MMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.unet = resunet(10, 10, block=HyperBottleneck, relu=CappingRelu(), ratio=-0, layers=6,
+        self.unet = resunet(10, 10, block=HyperBottleneck, relu=CappingRelu(), ratio=-0.5, layers=6,
                             vblks=[1, 1, 1, 1, 1, 1], hblks=[1, 1, 1, 1, 1, 1],
                             scales=[-1, -1, -1, -1, -1, -1], factors=[1, 1, 1, 1, 1, 1],
                             spatial=(64, 64))
